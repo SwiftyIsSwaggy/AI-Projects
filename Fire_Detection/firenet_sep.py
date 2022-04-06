1364f537b175ff11947fe1e1c03abdb3e59b5da7
John Pangas <swiftyxswaggy@outlook.com>
2022-04-06 07:22:36 +0000

    Edited the training script

--- Fire_Detection/firenet_sep.py
+++ Fire_Detection/firenet_sep.py
@@ -1,19 +1,3 @@
-# %% [markdown]
-# # The Fire Net Model
-
-# %% [markdown]
-# ## Using Depthwise Separable Convolution
-
-# %% [markdown]
-# This is my own model using depthwise separable convolutions. There is a version which has been designed before using normal standard convolution.
-
-# %% [markdown]
-# Therefore, for my project we have:
-# * A bench mark model which is a pre trained Mobile net.
-# * A fire net model made up of standard convolution.
-# * A fire net model made up of depthwise convolution.
-
-# %%
 import tensorflow as tf
 import numpy as np
 import os
@@ -21,40 +5,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob
 import shutil
-import tensorboard
-import tensorflow_hub as hub
+from tensorboard.plugins.hparams import api as hp
 
-# %%
+#---
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activation, BatchNormalization, DepthwiseConv2D, AveragePooling2D
 from sklearn.metrics import classification_report, confusion_matrix,roc_curve,auc, roc_auc_score,precision_recall_curve
 from sklearn.metrics import PrecisionRecallDisplay,RocCurveDisplay,ConfusionMatrixDisplay
 
+job_name = json.loads(os.environ.get("SM_TRAINING_ENV"))["job_name"]
+        logs_dir = "{}/{}".format(args.tf_logs_path, job_name)
+    logging.info("Writing TensorBoard logs to {}".format(logs_dir))
+    tf_writer = tf.summary.create_file_writer(logs_dir)
+    tf_writer.set_as_default()
 
-# %%
+#---
 _URL = 'https://fire-net-datasets.s3.amazonaws.com/Training_Dataset.zip'
 
 zip_file = tf.keras.utils.get_file(origin=_URL,extract=True)  
 #This will ge the file and extract it to a directory and extract to /Training Dataset
 
-# %%
+#---
 print(os.path.dirname(zip_file))
 #This function returns the directory of the extracted folder without the extracted folder inclusive
 
-# %%
+#---
 base_dir = os.path.join(os.path.dirname(zip_file), 'Training Dataset')
 #A good way to add the directory of the extracted folder and also the extracted folder itself.
 print(base_dir)
 
-# %%
+#---
 LOG_DIR = os.path.join(os.getcwd(), "Fire_Detection/logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
 
-# %%
+#---
 classes = ['Fire', 'NoFire']
 
-# %%
+#---
 for cl in classes:
   img_path = os.path.join(base_dir, cl)
   images = glob.glob(img_path + '/*.jpg')
@@ -71,15 +59,15 @@ for cl in classes:
       os.makedirs(os.path.join(base_dir, 'val', cl))
     shutil.move(v, os.path.join(base_dir, 'val', cl))
 
-# %%
+#---
 train_dir = os.path.join(base_dir, 'train')
 val_dir = os.path.join(base_dir, 'val')
 
-# %%
+#---
 batch_size = 100
 IMG_SHAPE = 128
 
-# %%
+#---
 image_gen_train = ImageDataGenerator(rescale = 1./255,
                     zoom_range = 0.5,
                     rotation_range=45,
@@ -97,7 +85,7 @@ print(train_data_gen.n)
 train_data_num = train_data_gen.samples
 #Find our size of datasets. each
 
-# %%
+#---
 # This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
 def plotImages(images_arr):
     fig, axes = plt.subplots(1, 5, figsize=(20,20))
@@ -107,11 +95,11 @@ def plotImages(images_arr):
     plt.tight_layout()
     plt.show()
 
-# %%
+#---
 augmented_images = [train_data_gen[0][0][0] for i in range(5)]
 plotImages(augmented_images)
 
-# %%
+#---
 image_gen_val = ImageDataGenerator(rescale=1./255)
 val_data_gen = image_gen_val.flow_from_directory(batch_size=batch_size,
                             directory=val_dir,
@@ -119,12 +107,60 @@ val_data_gen = image_gen_val.flow_from_directory(batch_size=batch_size,
                             class_mode='binary',
                             shuffle = False)
 
-# %%
+#---
 print(val_data_gen.samples)
 print(val_data_gen.n)
 val_data_num = val_data_gen.samples
 
-# %%
+def tb_plot_confusion_matrix(y_true, y_pred):
+    # Calculate the confusion matrix.
+    cm = confusion_matrix(y_true, y_pred)
+
+    classes_cnt = cm.shape[0]
+
+    figure = plt.figure(figsize=(8, 8))
+    plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+    plt.title("Confusion matrix")
+    plt.colorbar()
+    tick_marks = np.arange(classes_cnt)
+    plt.xticks(tick_marks, tick_marks, rotation=45)
+    plt.yticks(tick_marks, tick_marks)
+
+    # Normalize the confusion matrix.
+    cm = np.around(cm.astype("float") / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+
+    # Use white text if squares are dark; otherwise black.
+    threshold = cm.max() / 2.0
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        color = "white" if cm[i, j] > threshold else "black"
+        plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
+
+    plt.tight_layout()
+    plt.ylabel("True label")
+    plt.xlabel("Predicted label")
+
+    # Save the plot to a PNG in memory.
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png")
+    # Closing the figure prevents it from being displayed directly inside
+    # the notebook.
+    plt.close(figure)
+    buf.seek(0)
+    # Convert PNG buffer to TF image
+    image = tf.image.decode_png(buf.getvalue(), channels=4)
+    # Add the batch dimension
+    cm_image = tf.expand_dims(image, 0)
+
+    return cm_image
+
+#Hyperparamters to visualize in tensorboard
+HP_EPOCHS = hp.HParam("epochs", hp.IntInterval(1, 150))
+HP_BATCH_SIZE = hp.HParam("batch-size", hp.Discrete([64, 128, 256, 512]))
+HP_LR = hp.HParam("learning-rate", hp.RealInterval(0.0, 1.0))
+
+METRIC_ACCURACY = "accuracy"
+
+#---
 model = Sequential()
 
 model.add(Conv2D(filters = 32,kernel_size = 3, padding = "same", input_shape = (IMG_SHAPE, IMG_SHAPE, 3)), strides = 2)
@@ -223,7 +259,7 @@ model.add(AveragePooling2D(7,7), strides=(1,1))
 model.add(Flatten())
 model.add(Dense(2, activation = 'softmax'))
 
-# %% [markdown]
+#--- [markdown]
 # ### Places to Test
 # 1. Overfit first so that you see maximum number of epoch. (Overfits above 60)
 # 2. After overfitting, find the epoch where you want to reduce the learning rate. (This is 60 epoch)
@@ -233,12 +269,14 @@ model.add(Dense(2, activation = 'softmax'))
 # 6. After finding best epoch, use reduce early and stop. (Generalization)// No need, found the optimum range at about 60 epoch.
 # 7. Convert to depthwise separable convolution and increase layers.
 
-# %%
+#---
 model.summary()
 
 #Mobile Net had 2 million parameters
 
-# %%
+
+
+#---
 EPOCHS = 100
 model.compile(optimizer="adam",
                 loss=tf.keras.losses.sparse_categorical_crossentropy,
@@ -249,7 +287,7 @@ history = model.fit(train_data_gen,epochs= EPOCHS,
                 validation_data = val_data_gen,
                 validation_steps = int(np.ceil(val_data_gen.n / float(batch_size))))
 
-# %%
+#---
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
@@ -273,7 +311,7 @@ plt.title('Training and Validation Loss')
 plt.show()
 
 
-# %%
+#---
 #Run this on AWS 
 EFS_PATH_LOG_DIR = "/".join(LOG_DIR.strip("/").split('/')[1:-1])
 print (EFS_PATH_LOG_DIR)
@@ -291,12 +329,12 @@ https://<YOUR_URL>.studio.region.sagemaker.aws/jupyter/default/proxy/6006/
 
 """
 
-# %%
+#---
 #Run this on Colab or other places
 
 #Can run this before training to view the logs before training occurs or run after training to view after training
 
-# %%
+#---
 #Confusion Matrix and Classification Report
 val_data_gen.reset()
 Y_pred = model.predict(val_data_gen, int(np.ceil(val_data_gen.n / float(batch_size))))
@@ -307,7 +345,7 @@ print('Classification Report')
 target_names = ['Fire', 'No Fire']
 print(classification_report(val_data_gen.classes, y_pred, target_names=target_names))
 
-# %%
+#---
 #Confusion Matrix and Classification Report
 
 #Something is Wrong
@@ -320,12 +358,12 @@ print('Classification Report')
 target_names = ['Fire', 'No Fire']
 print(classification_report(val_data_gen.classes, y_pred, target_names=target_names))
 
-# %%
+#---
 cm = confusion_matrix(val_data_gen.classes, y_pred)
 
 cm_display = ConfusionMatrixDisplay(cm).plot()
 
-# %%
+#---
 fpr_keras, tpr_keras, thresholds_keras = roc_curve(val_data_gen.classes, y_pred)
 auc_keras = auc(fpr_keras, tpr_keras)
 
@@ -339,33 +377,33 @@ plt.title('ROC curve')
 plt.legend(loc='best')
 plt.show()
 
-# %%
+#---
 prec, recall, _ = precision_recall_curve(val_data_gen.classes, y_pred)
 pr_display = PrecisionRecallDisplay(precision=prec, recall=recall).plot()
 
-# %%
+#---
 model_name = "firenet"
 model_fullname = "{}_epochs_{}".format(model_name,EPOCHS)
 
-# %%
+#---
 export_path_keras = "./{}.h5".format(model_fullname)
 print(export_path_keras)
 
 model.save(export_path_keras)
 
-# %%
+#---
 export_path_sm = "./{}".format(model_fullname)
 print(export_path_sm)
 
 tf.saved_model.save(model, export_path_sm)
 
-# %%
+#---
 !ls {export_path_sm}
 
-# %%
+#---
 !zip -r {model_fullname}.zip {export_path_sm}
 
-# %%
+#---
 #Download saved model to Disk
 try:
   from google.colab import files
